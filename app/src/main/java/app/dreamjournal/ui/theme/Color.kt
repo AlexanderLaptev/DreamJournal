addfadc988cf3a50ed7de73bb0507a709c40c14e
ImmortalIdiot <max.krasulya@yandex.ru>
2025-01-19 02:01:30 +0300

    chore: rename the variables

--- app/src/main/java/app/dreamjournal/ui/theme/Color.kt
+++ app/src/main/java/app/dreamjournal/ui/theme/Color.kt
@@ -10,5 +10,5 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-val Text = Color(0xFFCAD3F5)
-val Overlay2 = Color(0xFF939AB7)
+val TextColor = Color(0xFFCAD3F5)
+val Overlay2Color = Color(0xFF939AB7)
