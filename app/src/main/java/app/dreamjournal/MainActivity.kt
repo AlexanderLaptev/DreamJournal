32cc8f0f614f4fdfbf1a819e2a896f8a41a725ed
ImmortalIdiot <max.krasulya@yandex.ru>
2025-01-19 02:40:34 +0300

    feat: add bottom navigation bar

--- app/src/main/java/app/dreamjournal/MainActivity.kt
+++ app/src/main/java/app/dreamjournal/MainActivity.kt
@@ -4,44 +4,32 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import app.dreamjournal.navigation.ApplicationNavigationFlow
+import app.dreamjournal.ui.components.navbars.BottomNavigationBar
 import app.dreamjournal.ui.theme.DreamJournalTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             DreamJournalTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                val navController = rememberNavController()
+
+                Scaffold(
+                    bottomBar = {
+                        BottomNavigationBar(navController = navController)
+                    }
+                ) { paddingValues ->
+                    ApplicationNavigationFlow(
+                        paddingValues = paddingValues,
+                        navController = navController
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DreamJournalTheme {
-        Greeting("Android")
     }
 }
