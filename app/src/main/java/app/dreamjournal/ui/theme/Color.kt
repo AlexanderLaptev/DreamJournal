530fd89b9433e8931720868dcffb3f3d32d0df2b
ImmortalIdiot <max.krasulya@yandex.ru>
2025-01-19 01:59:33 +0300

    feat: add nav bar items colors

--- app/src/main/java/app/dreamjournal/ui/theme/Color.kt
+++ app/src/main/java/app/dreamjournal/ui/theme/Color.kt
@@ -9,3 +9,6 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Text = Color(0xFFCAD3F5)
+val Overlay2 = Color(0xFF939AB7)
