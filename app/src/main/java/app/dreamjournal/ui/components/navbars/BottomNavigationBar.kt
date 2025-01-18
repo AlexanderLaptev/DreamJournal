f0e18d2d8d315a39b54480d36484898623671162
ImmortalIdiot <max.krasulya@yandex.ru>
2025-01-19 02:46:15 +0300

    fix: set the background color navbar item icon transparent; remove indicator color for the active nav bar item

--- app/src/main/java/app/dreamjournal/ui/components/navbars/BottomNavigationBar.kt
+++ app/src/main/java/app/dreamjournal/ui/components/navbars/BottomNavigationBar.kt
@@ -1,10 +1,14 @@
 package app.dreamjournal.ui.components.navbars
 
+import androidx.compose.foundation.background
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import androidx.navigation.NavHostController
@@ -36,11 +40,13 @@ fun BottomNavigationBar(
                 },
                 icon = {
                     Icon(
+                        modifier = Modifier.background(color = Color.Transparent),
                         imageVector = ImageVector.vectorResource(item.icon),
                         contentDescription = item.name,
                         tint = if (isSelected) { TextColor } else { Overlay2Color  }
                     )
-                }
+                },
+                colors = NavigationBarItemDefaults.colors(indicatorColor = Color.Transparent)
             )
         }
     }
