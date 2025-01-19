f0bd61acb45ecbabee8ff87581e5cd53964ea32f
ImmortalIdiot <max.krasulya@yandex.ru>
2025-01-20 02:23:29 +0300

    feat: add the tags colors and get dark and get light colors versions methods

--- app/src/main/java/app/dreamjournal/ui/theme/Color.kt
+++ app/src/main/java/app/dreamjournal/ui/theme/Color.kt
@@ -9,3 +9,30 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val TextColor = Color(0xFFCAD3F5)
+val DarkText = Color(0xFF394369)
+
+val Red = Color(0xFFED8796)
+val DarkRed = Color(0xFF601C26)
+
+val Blue = Color(0xFF8AADF4)
+val DarkBlue = Color(0xFF122D65)
+
+fun Color.getDarkVersion(color: Color): Color {
+    return when (color) {
+        TextColor -> DarkText
+        Red -> DarkRed
+        Blue -> DarkBlue
+        else -> color
+    }
+}
+
+fun Color.getLightVersion(color: Color): Color {
+    return when (color) {
+        DarkText -> TextColor
+        DarkRed -> Red
+        DarkBlue -> Blue
+        else -> color
+    }
+}
