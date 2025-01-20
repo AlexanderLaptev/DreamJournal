b6592888d48003f898176bab66517f9773628a6b
ImmortalIdiot <max.krasulya@yandex.ru>
2025-01-20 03:45:20 +0300

    fix: set min width and center the full text

--- app/src/main/java/app/dreamjournal/ui/components/TagView.kt
+++ app/src/main/java/app/dreamjournal/ui/components/TagView.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -14,7 +15,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import app.dreamjournal.ui.theme.TextColor
@@ -53,21 +56,54 @@ fun TagView(
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
-            Text(text = emoji, fontSize = emojiFontSize)
-            Spacer(modifier = Modifier.width(spaceBetweenEmojiAndText))
-            Text(text = tagText, fontSize = textFontSize, color = containerColor.getDarkVersion())
+            Text(
+                modifier = Modifier.isContentEmptyModifier(
+                    content = emoji,
+                    minWidth = tagViewHeight
+                ),
+                text = emoji,
+                textAlign = TextAlign.Center,
+                fontSize = emojiFontSize
+            )
+
+            if (emoji.isNotEmpty() && tagText.isNotEmpty()) {
+                Spacer(modifier = Modifier.width(spaceBetweenEmojiAndText))
+            }
+
+            Text(
+                modifier = Modifier.isContentEmptyModifier(
+                    content = tagText,
+                    minWidth = tagViewHeight
+                ),
+                text = tagText,
+                textAlign = TextAlign.Center,
+                fontSize = textFontSize,
+                color = containerColor.getDarkVersion()
+            )
         }
+    }
+}
+
+@Composable
+fun Modifier.isContentEmptyModifier(
+    content: String,
+    minWidth: Dp
+): Modifier {
+    return if (content.isBlank()) {
+        this
+    } else {
+        this.defaultMinSize(minWidth = minWidth)
     }
 }
 
 @Preview
 @Composable
 fun TagViewWithEmojiPreview() {
-    TagView(emoji = "😄", tagText = "with emoji", containerColor = TextColor)
+    TagView(emoji = "😄", tagText = "", containerColor = TextColor)
 }
 
 @Preview
 @Composable
 fun TagViewWithoutEmojiPreview() {
-    TagView(emoji = "", tagText = "without emoji", containerColor = TextColor)
+    TagView(emoji = "", tagText = "...", containerColor = TextColor)
 }
