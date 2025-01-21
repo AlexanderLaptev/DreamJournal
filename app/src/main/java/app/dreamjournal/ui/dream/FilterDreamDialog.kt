1273470a72534ebe26015bd4ad7fa57839b0ffc0
ImmortalIdiot <max.krasulya@yandex.ru>
2025-01-21 03:02:01 +0300

    chore: remove unused import

--- app/src/main/java/app/dreamjournal/ui/dream/FilterDreamDialog.kt
+++ app/src/main/java/app/dreamjournal/ui/dream/FilterDreamDialog.kt
@@ -18,7 +18,6 @@ import androidx.compose.material.icons.filled.Air
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material.icons.rounded.Brightness5
 import androidx.compose.material.icons.rounded.Brightness7
 import androidx.compose.material.icons.rounded.DarkMode
 import androidx.compose.material.icons.rounded.Star
