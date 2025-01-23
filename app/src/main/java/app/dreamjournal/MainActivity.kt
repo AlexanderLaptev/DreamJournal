a634174823a0ab3ccba45decac71b0a354896bd9
ImmortalIdiot <max.krasulya@yandex.ru>
2025-01-23 17:01:46 +0300

    feat: request the permissions

--- app/src/main/java/app/dreamjournal/MainActivity.kt
+++ app/src/main/java/app/dreamjournal/MainActivity.kt
@@ -1,9 +1,18 @@
 package app.dreamjournal
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
@@ -33,6 +42,7 @@ import app.dreamjournal.ui.theme.ApplicationTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        checkAndRequestPermissions()
         enableEdgeToEdge()
         setContent {
             ApplicationTheme {
@@ -82,5 +92,83 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    private fun checkAndRequestPermissions() {
+        when {
+            Build.VERSION.SDK_INT < Build.VERSION_CODES.Q -> {
+                requestPermissions(
+                    arrayOf(
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    )
+                )
+            }
+
+            Build.VERSION.SDK_INT < Build.VERSION_CODES.R -> {
+                requestPermissions(
+                    arrayOf(
+                        Manifest.permission.READ_EXTERNAL_STORAGE
+                    )
+                )
+            }
+
+            else -> {
+                if (!isExternalStorageManagerGranted()) {
+                    showManageStoragePermissionDialog()
+                }
+            }
+        }
+    }
+
+    private fun requestPermissions(permissions: Array<String>) {
+        val permissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permission ->
+            if (permission.values.all { it }) {
+                Toast.makeText(
+                    this,
+                    "All permissions is granted",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                Toast.makeText(
+                    this,
+                    "Permissions required for the application to work",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+        permissionLauncher.launch(permissions)
+    }
+
+    private fun isExternalStorageManagerGranted(): Boolean =
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            android.os.Environment.isExternalStorageManager()
+        } else {
+            true
+        }
+
+    // Replace alert dialog with composable
+    @RequiresApi(Build.VERSION_CODES.R)
+    private fun showManageStoragePermissionDialog() {
+        AlertDialog.Builder(this)
+            .setTitle("Access to storage")
+            .setMessage(
+                "File management access must be granted for the application to work.\n" +
+                        "Please, provide access in the settings"
+            )
+            .setPositiveButton("Provide access") { _, _ ->
+                val intent = Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION).apply {
+                    data = Uri.parse("package:$packageName")
+                }
+
+                startActivity(intent)
+            }
+            .setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .show()
     }
 }
