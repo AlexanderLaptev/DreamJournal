990d8e3c988423a0923cdeaf2bbd2805b9c17ae7
ImmortalIdiot <max.krasulya@yandex.ru>
2025-01-23 18:17:50 +0300

    refactor: use the odd format for voice messages for android sdk more than 28 api level

--- app/src/main/java/app/dreamjournal/logic/tag/audio/DreamAudioRecorder.kt
+++ app/src/main/java/app/dreamjournal/logic/tag/audio/DreamAudioRecorder.kt
@@ -4,22 +4,15 @@ import android.content.Context
 import android.media.MediaRecorder
 import android.os.Build
 import java.io.File
-import java.io.FileOutputStream
 
 class DreamAudioRecorder(private val context: Context) : AudioRecorder {
 
     private var audioRecorder: MediaRecorder? = null
 
     override fun start(outputFile: File) {
-        createMediaRecorder().apply {
-            setAudioSource(MediaRecorder.AudioSource.MIC)
-            setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
-            setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
-            setOutputFile(FileOutputStream(outputFile).fd)
-
+        createMediaRecorder(outputFile = outputFile).apply {
             prepare()
             start()
-
             audioRecorder = this
         }
     }
@@ -30,8 +23,23 @@ class DreamAudioRecorder(private val context: Context) : AudioRecorder {
         audioRecorder = null
     }
 
-    private fun createMediaRecorder(): MediaRecorder {
-        return if (Build.VERSION.SDK_INT >= 31) { MediaRecorder(context) }
-               else { MediaRecorder() }
+    private fun createMediaRecorder(outputFile: File): MediaRecorder {
+        val recorder = if (Build.VERSION.SDK_INT >= 31) {
+            MediaRecorder(context)
+        } else {
+            MediaRecorder()
+        }
+
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            recorder.setOutputFormat(MediaRecorder.OutputFormat.OGG)
+            recorder.setAudioEncoder(MediaRecorder.AudioEncoder.OPUS)
+        } else {
+            recorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+            recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
+        }
+        recorder.setOutputFile(outputFile.absolutePath)
+
+        return recorder
     }
 }
