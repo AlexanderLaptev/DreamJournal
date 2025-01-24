aad3dd40eb347150d571081d361b5c9122fdef61
Alexander Laptev <alexander_laptev4@inbox.ru>
2025-01-25 01:41:22 +0300

    feat: add a light color scheme

--- app/src/main/java/app/dreamjournal/ui/theme/Theme.kt
+++ app/src/main/java/app/dreamjournal/ui/theme/Theme.kt
@@ -3,6 +3,7 @@ package app.dreamjournal.ui.theme
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 
@@ -12,7 +13,26 @@ fun ApplicationTheme(
     useDarkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit,
 ) {
-    val colors = darkColorScheme(
+    val colors = if (useDarkTheme) darkColorScheme(
+        primary = CatppuccinColors.mauve,
+        onPrimary = CatppuccinColors.base,
+        primaryContainer = CatppuccinColors.mauve,
+        secondary = CatppuccinColors.mauve,
+        onSecondary = CatppuccinColors.base,
+        secondaryContainer = CatppuccinColors.mauve,
+        onSecondaryContainer = CatppuccinColors.text,
+        tertiary = CatppuccinColors.mauve,
+        onTertiary = CatppuccinColors.base,
+        surface = CatppuccinColors.base,
+        surfaceVariant = CatppuccinColors.base,
+        onSurface = CatppuccinColors.text,
+        onSurfaceVariant = CatppuccinColors.overlay2,
+        background = CatppuccinColors.base,
+        onBackground = CatppuccinColors.text,
+        surfaceContainer = CatppuccinColors.surface0,
+        surfaceContainerHigh = CatppuccinColors.surface0,
+        outline = CatppuccinColors.overlay2,
+    ) else lightColorScheme(
         primary = CatppuccinColors.mauve,
         onPrimary = CatppuccinColors.base,
         primaryContainer = CatppuccinColors.mauve,
