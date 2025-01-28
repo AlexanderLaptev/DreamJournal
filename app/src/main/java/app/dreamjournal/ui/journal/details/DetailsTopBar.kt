e58c28ea43308533d19d98ba4b8225bb7acde3fa
Alexander Laptev <alexander_laptev4@inbox.ru>
2025-01-28 16:17:06 +0300

    chore: cleanup DetailsTopBar

--- app/src/main/java/app/dreamjournal/ui/journal/details/DetailsTopBar.kt
+++ app/src/main/java/app/dreamjournal/ui/journal/details/DetailsTopBar.kt
@@ -1,6 +1,6 @@
 package app.dreamjournal.ui.journal.details
 
-import androidx.compose.foundation.layout.fillMaxWidth
+import android.content.res.Configuration
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.ArrowBack
 import androidx.compose.material.icons.rounded.Delete
@@ -9,83 +9,74 @@ import androidx.compose.material.icons.rounded.Star
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
+import app.dreamjournal.R
 import app.dreamjournal.ui.theme.ApplicationTheme
-import app.dreamjournal.ui.theme.CatppuccinColors
+import app.dreamjournal.ui.theme.FavoriteColor
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DetailsTopBar(
     isFavorite: Boolean = false,
-    onBackClick: () -> Unit,
-    onEditClick: () -> Unit,
-    onFavoriteClick: () -> Unit,
-    onRemoveClick: () -> Unit
+    onBack: () -> Unit = {},
+    onEdit: () -> Unit = {},
+    onFavorite: () -> Unit = {},
+    onDelete: () -> Unit = {},
 ) {
     TopAppBar(
-        colors = TopAppBarDefaults.topAppBarColors(
-            containerColor = CatppuccinColors.base,
-            titleContentColor = CatppuccinColors.text,
-            navigationIconContentColor = CatppuccinColors.text,
-            actionIconContentColor = CatppuccinColors.subtext0
-        ),
         title = {
             Text(
-                text = "Details",
-                fontSize = 22.sp,
-                modifier = Modifier.fillMaxWidth()
+                text = stringResource(R.string.screen_details),
             )
         },
+
         navigationIcon = {
-            IconButton(onClick = { onBackClick() }) {
+            IconButton(onClick = onBack) {
                 Icon(
                     imageVector = Icons.AutoMirrored.Rounded.ArrowBack,
-                    contentDescription = "Go back"
+                    contentDescription = stringResource(R.string.button_back_desc)
                 )
             }
         },
+
         actions = {
-            IconButton(onClick = { onEditClick() }) {
+            IconButton(onClick = onEdit) {
                 Icon(
                     imageVector = Icons.Rounded.Edit,
-                    contentDescription = "Edit"
+                    contentDescription = stringResource(R.string.button_edit_desc)
                 )
             }
 
-            IconButton(onClick = { onFavoriteClick() }) {
+            IconButton(onClick = onFavorite) {
                 Icon(
                     imageVector = Icons.Rounded.Star,
-                    contentDescription = "Favorite",
-                    tint = if (isFavorite) CatppuccinColors.yellow else CatppuccinColors.subtext0
+                    contentDescription = stringResource(R.string.button_favorite_desc),
+                    tint = if (isFavorite) FavoriteColor else LocalContentColor.current,
                 )
             }
 
-            IconButton(onClick = { onRemoveClick() }) {
+            IconButton(onClick = onDelete) {
                 Icon(
                     imageVector = Icons.Rounded.Delete,
-                    contentDescription = "Delete"
+                    contentDescription = stringResource(R.string.button_delete_desc)
                 )
             }
         }
     )
 }
 
-@Preview
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_NO)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
 fun Preview() {
     ApplicationTheme {
         DetailsTopBar(
             isFavorite = true,
-            onBackClick = {},
-            onEditClick = {},
-            onFavoriteClick = {},
-            onRemoveClick = {}
         )
     }
 }
--- app/src/main/java/app/dreamjournal/ui/theme/Color.kt
+++ app/src/main/java/app/dreamjournal/ui/theme/Color.kt
@@ -1,5 +1,6 @@
 package app.dreamjournal.ui.theme
 
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
 private typealias CatppuccinColor = com.catppuccin.Color
@@ -8,3 +9,7 @@ fun CatppuccinColor.toCompose(): Color {
     // FIXME: bug in Catppuccin#29, green and blue are swapped
     return Color(red = this.r(), green = this.b(), blue = this.g())
 }
+
+val AccentColor @Composable get() = CatppuccinColors.mauve
+val FavoriteColor @Composable get() = CatppuccinColors.yellow
+val LucidColor @Composable get() = CatppuccinColors.mauve
